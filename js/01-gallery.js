bd5d1c05a3256d0ba37d43e994094bd8820c5c94
Kateryna Kleshch <ketrins1993@gmail.com>
2022-08-08 19:52:27 +0300

    add 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,54 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const createPhotoGallary = photos => {
+  return photos
+    .map(({ preview, original, description }) => {
+      return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+            </div>
+        `;
+    })
+    .join('');
+};
+
+const galleryMarkup = createPhotoGallary(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const ongalleryContainerClick = evt => {
+  evt.preventDefault();
+  console.log(evt.target.nodeName);
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  const realPicture = evt.target.dataset.source;
+  const instance = basicLightbox.create(`
+        <div class="modal">
+            <img src="${realPicture}" />
+        </div>
+    `);
+
+  instance.show();
+  console.log(realPicture);
+  console.log(instance.visible());
+  console.log(instance.element());
+  const el = instance.element();
+  el.addEventListener('click', () => {
+    // if (!instance.visible()) {
+    //     return
+    // }
+    instance.close();
+  });
+};
+
+galleryContainer.addEventListener('click', ongalleryContainerClick);

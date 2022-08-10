6608e99607d79f567553363413fbc3338698a837
Kateryna Kleshch <ketrins1993@gmail.com>
2022-08-10 13:03:25 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,13 +7,15 @@ const createPhotoGallary = photos => {
   return photos
     .map(({ preview, original, description }) => {
         return `
-            <a class="gallery__item" href="${original}">
-                <img 
-                class="gallery__image" 
-                src="${preview}" 
-                alt="${description}"
-                />
-            </a>
+            <li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img 
+                    class="gallery__image" 
+                    src="${preview}" 
+                    alt="${description}"
+                    />
+                </a>
+            </li>
         `;
     })
     .join('');
